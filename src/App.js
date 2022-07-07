40fe2a8f4680126ebb0e86c26d250b88996e98d3
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-08 00:22:48 +0530

    Why we need useCallback Child component is called even though the other state is being changed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import { memo, useMemo, useState } from "react";
 import "./styles.css";
 
-const Block = ({ changeObject, handler }) => {
+const Block = ({ changeObject }) => {
   console.log("re-render");
   const value = changeObject?.val;
   return (
     <div
-      onClick={() => handler()}
       style={{ width: 300, height: 300, background: value ? "red" : "blue" }}
     ></div>
   );
 };
 
-const SmallBlock = () => {
+const SmallBlock = ({ dummy, handler }) => {
   console.log("Small child was re-rendered");
   return <h1>Small Child</h1>;
 };
 const MemoizedSmall = memo(SmallBlock);
 export default function App() {
   const [change, setChange] = useState(false);
+  const [dummy, setDummy] = useState([]);
   // const [changeObject, setChangeObject] = useState({ val: false });
   const memoizedResult = useMemo(() => {
     return assign(change);
@@ -37,9 +37,9 @@ export default function App() {
       <h2>React Memoization Practice</h2>
       <button onClick={() => setChange(true)}>SET</button>
       <button onClick={() => setChange(!change)}>TOGGLE</button>
-      <Block changeObject={memoizedResult} handler={handler} />
+      {/* <Block changeObject={memoizedResult} /> */}
       {/* <SmallBlock /> */}
-      <MemoizedSmall />
+      <MemoizedSmall dummy={dummy} handler={handler} />
     </div>
   );
 }
